0627231430d37ba6ec392515348314f6e44847e5
Pedro Lucas <78593920+perepos@users.noreply.github.com>
2022-06-03 21:31:37 -0300

    Add files via upload

---
+++ Desafios de Código/Conversor de Temperatura/ConversorTemperatura/ConversorTemperatura/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Conversor
+{
+
+    public class Converte
+    {
+        public static void Main (string[] args)
+        {
+            double c, media;
+
+            Console.WriteLine("Insira o valor em Celsius que deseja converter: ");
+
+            c = double.Parse(Console.ReadLine());
+
+            media = (c * 9 / 5) + 32;
+
+            Console.WriteLine("Valor convertido: {0}", media);
+
+        }
+
+    }
+
+}
---
+++ Desafios de Código/Volume Caixa Retangular/Volume_Caixa_Retangular/Volume_Caixa_Retangular/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CaixaVolume
+{
+
+    public class VolumeCaixa
+    {
+        public static void Main(string[] agrs)
+        {
+            double mediaV, comprimento, largura, altura;
+
+            Console.WriteLine("Insira o Comprimento do Retângulo");
+            comprimento = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o Largura do Retângulo");
+            largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o Largura do Retângulo");
+            altura = double.Parse(Console.ReadLine());
+
+            mediaV = comprimento * largura * altura;
+
+            //Para inserir o metro cúbico é ALT + 0179
+            Console.WriteLine($"O volume do retângulo é: {mediaV}³");
+        }
+    }
+}
